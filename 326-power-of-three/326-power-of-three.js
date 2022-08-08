3dcee7cf67f1fa43eb2dbfb78c6472359e4bf2d6
Shaban-Eissa <shabaneissa767@gmail.com>
2022-08-08 19:16:52 +0200

    Time: 345 ms (46.44%), Space: 51.2 MB (59.58%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+if(n == 1) return true
+  if(n%3 != 0 || n==0) return false;
+  return isPowerOfThree(n/3)    
+};
