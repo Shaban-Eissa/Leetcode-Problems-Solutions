f34e48e3b5ebf71a508692707523fa485627216f
Shaban-Eissa <shabaneissa767@gmail.com>
2022-08-11 18:01:00 +0200

    Time: 134 ms (25.91%), Space: 44.8 MB (90.51%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    var firstArr = nums;
+  var secondArr = nums;
+  return firstArr.concat(secondArr);
+};
