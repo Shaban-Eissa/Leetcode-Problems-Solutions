d1f5f70ad0149a48c3389d155c018e119fc91a75
Shaban-Eissa <shabaneissa767@gmail.com>
2022-08-12 17:59:55 +0200

    Time: 121 ms (24.80%), Space: 44.3 MB (77.16%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    var result = [] 
+  
+  for(let i = 0 ; i < s.length ; i++) {
+    result[indices[i]] = s[i]
+  }
+
+  return result.join('')
+};
