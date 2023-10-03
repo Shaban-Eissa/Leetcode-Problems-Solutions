53ca43d7b641192e41fe14e189fa962ca6a07b4d
Shaban-Eissa <shabaneissa767@gmail.com>
2023-10-03 17:01:47 +0300

    Time: 78 ms (13.31%), Space: 44.8 MB (27.38%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var frequencySort = function (nums) {
+    const map = new Map();
+    for (let n of nums) {
+        map.set(n, (map.get(n) + 1) || 1)
+    }
+    return nums.sort((a, b) => map.get(a) - map.get(b) || b - a)
+};
