fd359188d2d8da00567ee43b95e16a76d5ac3efa
Shaban-Eissa <shabaneissa767@gmail.com>
2023-10-03 11:54:58 +0300

    Time: 53 ms (82.67%), Space: 43.8 MB (62.57%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.js
+++ 0496-next-greater-element-i/0496-next-greater-element-i.js
@@ -3,26 +3,20 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
+
 var nextGreaterElement = function (nums1, nums2) {
 
     let stack = []
-    for (let i = 0; i < nums1.length; i++) {
-        let index = nums2.indexOf(nums1[i]);
-        if (index === nums2.length - 1) {
-            stack.push(-1)
-        }
-        else {
-            for (let j = index + 1; j < nums2.length; j++) {
-                if (nums2[j] > nums1[i]) {
-                    stack.push(nums2[j]);
-                    break;
-                }
-                else if (j === nums2.length - 1 && nums2[j] <= nums1[i]) {
-                    stack.push(-1);
-                    break;
-                }
+
+    for (let num of nums1) {
+        let flag = -1;
+        for (let i = nums2.indexOf(num) + 1; i < nums2.length; i++) {
+            if (nums2[i] > num) {
+                flag = nums2[i];
+                break;
             }
         }
+        stack.push(flag);
     }
     return stack;
 
