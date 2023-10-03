78d4e8fa86606b86468fe197848a809071e07218
Shaban-Eissa <shabaneissa767@gmail.com>
2023-10-03 19:24:08 +0300

    Time: 62 ms (61.82%), Space: 43.3 MB (58.08%) - LeetHub

---
+++ 2629-function-composition/2629-function-composition.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function (functions) {
+
+    return function (x) {
+        return functions.reduceRight((acc, fn) => {
+            return fn(acc);
+        }, x);
+    }
+};
+
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
