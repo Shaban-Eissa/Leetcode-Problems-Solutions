9f236b8972ccf048c4a9eec081770effe79b0281
Shaban-Eissa <shabaneissa767@gmail.com>
2023-10-03 19:10:45 +0300

    Time: 41 ms (96.41%), Space: 42 MB (43.15%) - LeetHub

---
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function (arr, fn) {
+
+    let resultArray = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            resultArray.push(arr[i]);
+        }
+    }
+    return resultArray;
+};
