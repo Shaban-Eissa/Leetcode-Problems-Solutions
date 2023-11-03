f4c9f0fd0a752c109265b675fb1f15044bdaed0b
Shaban-Eissa <shabaneissa767@gmail.com>
2023-11-03 14:01:55 +0200

    Time: 106 ms (39.99%), Space: 47.7 MB (80.69%) - LeetHub

---
+++ 2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[][]}
+ */
+function binarySearch(arr, target) {
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start <= end) {
+        let mid = Math.floor((start + end) / 2);
+
+        if (arr[mid] === target) {
+            return mid;
+        } else if (arr[mid] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+
+
+var findDifference = function (nums1, nums2) {
+
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
+
+    let FinalResult = [];
+    let FirstPart = []
+    let secondPart = []
+
+    for (let num of nums1) {
+        if (binarySearch(nums2, num) == -1 && !FirstPart.includes(num)) {
+            FirstPart.push(num);
+        }
+    }
+
+    for (let nums of nums2) {
+        if (binarySearch(nums1, nums) == -1 && !secondPart.includes(nums)) {
+            secondPart.push(nums);
+        }
+    }
+    FinalResult.push(FirstPart);
+    FinalResult.push(secondPart);
+    return FinalResult;
+};
