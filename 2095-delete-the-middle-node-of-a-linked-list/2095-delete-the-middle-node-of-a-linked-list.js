230bda585cb1e7b9ff9860394ce765489fe611dd
Shaban-Eissa <shabaneissa767@gmail.com>
2024-02-17 16:08:10 +0200

    Time: 710 ms (15.38%), Space: 88.2 MB (97.24%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.js
@@ -10,18 +10,28 @@
  * @return {ListNode}
  */
 var deleteMiddle = function (head) {
-    if (!head.next) return null;
-    let len = 0, curr = head;
-    while (curr) {
-        len++;
-        curr = curr.next;
-    }
-    len = Math.floor(len / 2);
-    curr = head;
-    while (len - 1) {
-        curr = curr.next;
-        len--;
-    }
-    curr.next = curr.next.next;
-    return head;
+  if (head.next === null) {
+    return null;
+  }
+  let current = head;
+  let length = 0;
+  while (current !== null) {
+    current = current.next;
+    length += 1;
+  }
+  let middle = Math.floor(length / 2);
+  let previous = traverseToMiddle(middle - 1, head);
+  let unwantedNode = previous.next;
+  previous.next = unwantedNode.next;
+  return head;
+};
+
+const traverseToMiddle = (index, head) => {
+  let counter = 0;
+  let currentNode = head;
+  while (counter !== index) {
+    currentNode = currentNode.next;
+    counter += 1;
+  }
+  return currentNode;
 };
