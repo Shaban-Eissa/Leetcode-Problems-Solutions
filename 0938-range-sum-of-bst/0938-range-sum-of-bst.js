564b3a128496dc2c0ca5c3250a48baa0153b5d30
Shaban-Eissa <shabaneissa767@gmail.com>
2024-02-20 16:02:39 +0200

    Time: 155 ms (25.62%), Space: 81.1 MB (83.73%) - LeetHub

---
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.js
@@ -0,0 +1,19 @@
+var rangeSumBST = function (root, low, high) {
+  let sum = 0;
+  let stack = [root];
+  while (stack.length > 0) {
+    let node = stack.pop();
+    if (node !== null) {
+      if (node.val >= low && node.val <= high) {
+        sum += node.val;
+      }
+      if (low < node.val) {
+        stack.push(node.left);
+      }
+      if (node.val < high) {
+        stack.push(node.right);
+      }
+    }
+  }
+  return sum;
+};
