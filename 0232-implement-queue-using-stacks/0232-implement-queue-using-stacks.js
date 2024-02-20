3d13620966403e5a6843686e88a3927094f81975
Shaban-Eissa <shabaneissa767@gmail.com>
2024-02-20 13:34:25 +0200

    Time: 56 ms (26.34%), Space: 48 MB (80.81%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -0,0 +1,32 @@
+class MyQueue {
+  constructor() {
+    this.stack1 = [];
+    this.stack2 = [];
+  }
+
+  push(x) {
+    this.stack1.push(x);
+  }
+
+  pop() {
+    if (this.stack2.length === 0) {
+      while (this.stack1.length > 0) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+    return this.stack2.pop();
+  }
+
+  peek() {
+    if (this.stack2.length === 0) {
+      while (this.stack1.length > 0) {
+        this.stack2.push(this.stack1.pop());
+      }
+    }
+    return this.stack2[this.stack2.length - 1];
+  }
+
+  empty() {
+    return this.stack1.length === 0 && this.stack2.length === 0;
+  }
+}
