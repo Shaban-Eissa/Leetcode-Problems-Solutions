2f478f30479b969dd895bc605c3b42875b8a0b72
Shaban-Eissa <shabaneissa767@gmail.com>
2024-02-22 15:01:11 +0200

    Time: 97 ms (26.29%), Space: 56.3 MB (29.27%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = nums[i] * nums[i];
+  }
+  return nums.sort((a, b) => a - b);
+};
