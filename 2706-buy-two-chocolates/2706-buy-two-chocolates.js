246e97daef50d5942844379280311417547f36d3
Shaban-Eissa <shabaneissa767@gmail.com>
2024-02-22 15:21:37 +0200

    Time: 100 ms (36.61%), Space: 53.7 MB (28.87%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
+var buyChoco = function (prices, money) {
+  prices.sort((a, b) => a - b);
+
+  if (money - (prices[0] + prices[1]) < 0) return money;
+  return money - (prices[0] + prices[1]);
+};
+
