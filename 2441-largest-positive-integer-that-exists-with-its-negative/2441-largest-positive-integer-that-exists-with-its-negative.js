0c019698c0db94f3dc22ab1f7e2c9df8f930932f
Shaban-Eissa <shabaneissa767@gmail.com>
2024-05-02 05:48:13 +0300

    Time: 54 ms (98.70%), Space: 51.8 MB (38.70%) - LeetHub

--- 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.js
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.js
@@ -2,12 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMaxK = function (nums) {
-  let max = [];
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] < 0 && nums.includes(-nums[i])) {
-      max.push(-nums[i]);
+var findMaxK = function(nums) {
+    let answer = -1 
+    let seen = new Set()
+
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(-nums[i])) {
+            answer = Math.max(answer, Math.abs(nums[i]))
+        }
+        seen.add(nums[i])
     }
-  }
-  return max.length > 0 ? Math.max(...max) : -1;
+    return answer
 };
