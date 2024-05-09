f2310373ed2df6bb26493da5d430355b4387d45d
Shaban-Eissa <shabaneissa767@gmail.com>
2024-05-09 13:44:13 +0300

    Time: 64 ms (98.90%), Space: 64 MB (43.81%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "*") {
+      stack.pop();
+    } else {
+      stack.push(s[i]);
+    }
+  }
+  return stack.join("");
+};
