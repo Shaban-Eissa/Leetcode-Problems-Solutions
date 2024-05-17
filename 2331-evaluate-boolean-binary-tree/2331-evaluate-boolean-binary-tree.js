189e577ab6de0c87f708e491b1eea4d05c2b1de0
Shaban-Eissa <shabaneissa767@gmail.com>
2024-05-17 11:24:02 +0300

    Time: 68 ms (31.41%), Space: 55.7 MB (49.74%) - LeetHub

---
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+var evaluateTree = (root) =>
+  root.val === 3
+    ? evaluateTree(root.left) & evaluateTree(root.right)
+    : root.val === 2
+    ? evaluateTree(root.left) | evaluateTree(root.right)
+    : root.val;
