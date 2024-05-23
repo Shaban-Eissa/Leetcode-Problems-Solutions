daf4ae87d97d7a530303deadf6d5b772a3e6745c
Shaban-Eissa <shabaneissa767@gmail.com>
2024-05-23 10:13:30 +0300

    Time: 321 ms (82.98%), Space: 54 MB (72.34%) - LeetHub

---
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var isSafe = function (nums, part, j, k) {
+    for (let i = 0; i < part.length; i++) {
+        if (Math.abs(nums[j] - part[i]) == k) {
+            return false;
+        }
+    }
+    return true;
+}
+
+
+var beautifulSubsets = function (nums, k) {
+    count = 0;
+    part = [];
+
+    var subSetArr = function (i) {
+        if (i >= nums.length) {
+            count++;
+            return;
+        }
+        if (isSafe(nums, part, i, k)) {
+            part.push(nums[i]);
+            subSetArr(i + 1);
+            part.pop();
+        }
+        subSetArr(i + 1);
+    }
+
+    subSetArr(0);
+    return count - 1;
+};
