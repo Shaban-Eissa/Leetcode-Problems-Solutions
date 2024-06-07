7a1a43410ec6a07292f33d1b453e4662552801d5
Shaban-Eissa <shabaneissa767@gmail.com>
2024-06-07 12:04:29 +0300

    Time: 69 ms (51.97%), Space: 52.9 MB (67.24%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+class ListNode {
+  constructor(val, next) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+var oddEvenList = function (head) {
+  if (!head) return head;
+  let odd = head;
+  let even = head.next;
+  let evenHead = even;
+  while (even && even.next) {
+    odd.next = even.next;
+    odd = odd.next;
+    even.next = odd.next;
+    even = even.next;
+  }
+  odd.next = evenHead;
+  return head;
+};
