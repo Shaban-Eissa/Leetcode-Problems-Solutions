c02171091552605ca0831f50f13571f2bc68887e
Shaban-Eissa <shabaneissa767@gmail.com>
2024-06-10 21:13:25 +0300

    Time: 47 ms (87.11%), Space: 49 MB (65.10%) - LeetHub

--- 1051-height-checker/1051-height-checker.js
+++ 1051-height-checker/1051-height-checker.js
@@ -2,15 +2,13 @@
  * @param {number[]} heights
  * @return {number}
  */
-var heightChecker = function(heights) {
-    var copy = [...heights]
-  copy.sort((a,b) => a-b)
-
-  var count = 0
-  for(let i = 0 ; i < heights.length ; i++) {
-    if(heights[i] !== copy[i]) {
-      count += 1
+var heightChecker = function (heights) {
+  let sorted = [...heights].sort((a, b) => a - b);
+  let count = 0;
+  for (let i = 0; i < heights.length; i++) {
+    if (heights[i] !== sorted[i]) {
+      count++;
     }
   }
-  return count
+  return count;
 };
