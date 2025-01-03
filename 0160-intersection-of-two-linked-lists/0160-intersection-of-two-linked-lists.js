3467105ef08ed1977074452afc7759d4b159a7ba
Shaban-Eissa <shabaneissa767@gmail.com>
2025-01-03 19:55:03 +0200

    Time: 75 ms (75.49%), Space: 58 MB (79.96%) - LeetHub

---
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} headA
+ * @param {ListNode} headB
+ * @return {ListNode}
+ */
+var getIntersectionNode = function (headA, headB) {
+    let currentA = headA;
+    let currentB = headB;
+    while (currentA !== currentB) {
+        currentA = currentA === null ? headB : currentA.next;
+        currentB = currentB === null ? headA : currentB.next;
+    }
+    return currentA;
+};
