4c59b10f598a569cb0079474737d5f59c31bc279
Shaban-Eissa <shabaneissa767@gmail.com>
2025-01-19 18:24:56 +0200

    Time: 10 ms (48.14%), Space: 51.4 MB (32.31%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -4,15 +4,26 @@
  * @return {number[]}
  */
 var twoSum = function (nums, target) {
-  let result = [];
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (nums[i] + nums[j] === target) {
-        result.push(i, j);
-      }
+    const numsWithIndex = nums.map((num, index) => ({ num, index }));
+    numsWithIndex.sort((a, b) => a.num - b.num);
+
+    let left = 0;
+    let right = numsWithIndex.length - 1;
+
+    while (left < right) {
+        const sum = numsWithIndex[left].num + numsWithIndex[right].num;
+        if (sum === target) {
+            return [numsWithIndex[left].index, numsWithIndex[right].index];
+        } else if (sum < target) {
+            left++;
+        } else {
+            right--;
+        }
     }
-  }
-  return result;
+
+    return [];
 };
 
 console.log(twoSum([3, 2, 4], 6));
+
+
