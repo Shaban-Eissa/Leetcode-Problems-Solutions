ecd78ab3bfa7567d655d0efac3f4c5bba2d3da9b
Shaban-Eissa <shabaneissa767@gmail.com>
2025-02-08 20:00:27 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.js
@@ -4,17 +4,38 @@
  * @return {number}
  */
 var timeRequiredToBuy = function (tickets, k) {
-    let countTime = 0;
-    while (tickets[k] > 0) {
-        for (let i = 0; i < tickets.length; i++) {
-            if (tickets[k] == 0) {
-                return countTime;
+    let time = 0;
+    let n = tickets.length;
+    let queue = [];
+
+    for (let i = 0; i < n; i++) {
+        enqueue(queue, [tickets[i], i]);
+    }
+
+    while (queue.length > 0) {
+        let [ticket, index] = dequeue(queue);
+
+        ticket -= 1;
+        time += 1;
+        if (ticket === 0) {
+            k -= 1;
+            if (k === 0) {
+                break;
             }
-            if (tickets[i] > 0) {
-                tickets[i] -= 1;
-                countTime += 1;
-            }
+        } else {
+            enqueue(queue, [ticket, index]);
         }
     }
-    return countTime
+    return time;
 };
+
+const dequeue = (queue) => {
+    if (queue.length === 0) return null;
+    return queue.shift();
+};
+
+const enqueue = (queue, item) => {
+    queue.push(item);
+};
+
+console.log(timeRequiredToBuy([2, 3, 2], 2)); // 6
