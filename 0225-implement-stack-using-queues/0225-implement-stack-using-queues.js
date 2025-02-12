9aef5ec63122afe0c2aa0cfd5c931c4acdbe900f
Shaban-Eissa <shabaneissa767@gmail.com>
2025-02-12 19:45:42 +0200

    Time: 0 ms (100%), Space: 49.1 MB (17.44%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.js
@@ -1,36 +1,46 @@
-class MyStack {
-  constructor() {
+var MyStack = function () {
     this.queue1 = [];
     this.queue2 = [];
-  }
+};
 
-  push(x) {
-    this.queue1.push(x);
-  }
-  pop() {
-    while (this.queue1.length > 1) {
-      this.queue2.push(this.queue1.shift());
+/**
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function (x) {
+    this.queue2.push(x);
+    while (this.queue1.length > 0) {
+        this.queue2.push(this.queue1.shift());
     }
-    const result = this.queue1.shift();
-    while (this.queue2.length > 0) {
-      this.queue1.push(this.queue2.shift());
-    }
-    return result;
-  }
+    [this.queue1, this.queue2] = [this.queue2, this.queue1];
+};
 
-  top() {
-    while (this.queue1.length > 1) {
-      this.queue2.push(this.queue1.shift());
-    }
-    const result = this.queue1.shift();
-    this.queue2.push(result);
-    while (this.queue2.length > 0) {
-      this.queue1.push(this.queue2.shift());
-    }
-    return result;
-  }
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function () {
+    return this.queue1.shift();
+};
 
-  empty() {
-    return this.queue1.length === 0 && this.queue2.length === 0;
-  }
-}
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function () {
+    return this.queue1[0];
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function () {
+    return this.queue1.length === 0;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
